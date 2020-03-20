ab947908a8628c9a8759bbc0567663a0ac66303c
klunkean <andre.klunker@posteo.de>
2020-03-20 15:19:51 +0100

    Add __str__ and __repr__ to Spieler class

--- src/schocken/spieler.py
+++ src/schocken/spieler.py
@@ -6,3 +6,9 @@ class Spieler():
         self.aktiv = False
         self.augen = 0
         self.anzahl_wuerfe = 0
+
+    def __str__(self):
+        return f"<Spieler {self.name}, Deckel: {self.deckel}>"
+
+    def __repr__(self):
+        return self.__str__()
