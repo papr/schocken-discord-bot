d83ec1471e1cd1fbcd29970aadaba6f41926116c
CGentsch <chgentsch@googlemail.com>
2020-03-20 15:58:39 +0100

    changes to wuerfel.py

--- src/schocken/wuerfel.py
+++ src/schocken/wuerfel.py
@@ -6,12 +6,8 @@ class ZuVieleWuerfel(Exception):
 
 
 def werfen(anzahl):
-    if anzahl == 3:
-        return sorted(tuple(randint(1, 6) for _ in range(3)), reverse=True)
-    elif anzahl == 2:
-        return sorted(tuple(randint(1, 6) for _ in range(2)), reverse=True)
-    elif anzahl == 1:
-        return tuple(randint(1, 6) for _ in range(1))
+    if anzahl <= 3:
+        return sorted(tuple(randint(1, 6) for _ in range(anzahl)), reverse=True)
     else:
         raise ZuVieleWuerfel(
             f"Es dürfen maximal 3 Würfel geworfen werden! Nicht {anzahl}."
