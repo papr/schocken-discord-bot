7e2676ba9e06448c8b1b1aafe7fb6655a47d02b9
Pablo Prietz <pablo@prietz.org>
2020-03-28 23:08:05 +0100

    Tests fixen

--- tests/test_einwerfen.py
+++ tests/test_einwerfen.py
@@ -1,137 +1,162 @@
 import pytest
 
-from schocken.schockenrunde import Einwerfen, SchockenRunde
+from schocken import events
+from schocken.schockenrunde import SchockenRunde
 from schocken.spieler import Spieler
 from schocken.exceptions import FalscheAktion, FalscherSpieler
 
 from schocken import wuerfel
 
-runde = SchockenRunde()
-assert runde.state == "einwerfen"
-# testspieler
-spieler_1 = Spieler("spieler_1")
-spieler_2 = Spieler("spieler_2")
-spieler_3 = Spieler("spieler_3")
-spieler_4 = Spieler("spieler_4")
 
-# FALL: Zwei Spieler werfen dasselbe ein, einer versucht zu starten
-wuerfel.werfen = lambda n: (1,) * n
-runde.command_to_event(spieler_1.name, "einwerfen")
-runde.command_to_event(spieler_2.name, "einwerfen")
-
-with pytest.raises(FalscheAktion):
-    runde.command_to_event(spieler_1.name, "wuerfeln")
-
-# FALL: spieler 2 würfelt eindeutig das niedrigste
-runde = SchockenRunde()
-assert runde.state == "einwerfen"
-wuerfel.werfen = lambda n: (2,) * n
-runde.command_to_event(spieler_1.name, "einwerfen")
-wuerfel.werfen = lambda n: (1,) * n
-runde.command_to_event(spieler_2.name, "einwerfen")
-wuerfel.werfen = lambda n: (3,) * n
-runde.command_to_event(spieler_3.name, "einwerfen")
-
-# Richtige exception, wenn jemand anders anfangen will?
-with pytest.raises(FalscherSpieler):
-    runde.command_to_event(spieler_1.name, "wuerfeln")
-
-# Richtige exception, wenn jemand stechen will.
-with pytest.raises(FalscheAktion):
-    runde.command_to_event(spieler_2.name, "stechen")
-
-# momentaner state ist einwerfen
-assert runde.state == "einwerfen"
-
-# spieler_2 wuerfelt, danach muss einwerfen fertig sein
-runde.command_to_event(spieler_2.name, "wuerfeln")
-assert runde.state == "wuerfeln"
+@pytest.fixture
+def spieler(n=4):
+    # testspieler
+    return [Spieler(f"spieler_{i+1}") for i in range(n)]
 
 
-# FALL mehrfaches stechen, erst 4, dann 3, dann 2 spieler, dann beginn
-runde = SchockenRunde()
-assert runde.state == "einwerfen"
-wuerfel.werfen = lambda n: (1,) * n
-runde.command_to_event(spieler_1.name, "einwerfen")
-runde.command_to_event(spieler_2.name, "einwerfen")
-runde.command_to_event(spieler_3.name, "einwerfen")
-runde.command_to_event(spieler_4.name, "einwerfen")
-# alle stecher in der liste?
-assert [st.name for st in runde.einwerfen.stecher_liste] == [
-    "spieler_1",
-    "spieler_2",
-    "spieler_3",
-    "spieler_4",
-]
-# neues stechen, 3 spieler werfen 1, spieler_4 wirft 2
-runde.command_to_event(spieler_1.name, "stechen")
-assert runde.state == "stechen"
-runde.command_to_event(spieler_2.name, "stechen")
-runde.command_to_event(spieler_3.name, "stechen")
-wuerfel.werfen = lambda n: (2,) * n
-runde.command_to_event(spieler_4.name, "stechen")
-# spieler 4 versucht unerlaubterweise zu stechen
-with pytest.raises(FalscherSpieler):
-    runde.command_to_event(spieler_4.name, "stechen")
+def test_einwerfen_trotz_stechen(spieler):
+    runde = SchockenRunde()
+    assert runde.leaf_state.name == "einwerfen"
 
-# die anderen spieler stechen
-wuerfel.werfen = lambda n: (1,) * n
-runde.command_to_event(spieler_1.name, "stechen")
-runde.command_to_event(spieler_2.name, "stechen")
-wuerfel.werfen = lambda n: (2,) * n
-runde.command_to_event(spieler_3.name, "stechen")
-# nur noch 1 und 2 in der stecherliste
-assert [st.name for st in runde.einwerfen.stecher_liste] == ["spieler_1", "spieler_2"]
+    # FALL: Zwei Spieler werfen dasselbe ein, einer versucht zu starten
+    wuerfel.werfen = lambda n: (1,) * n
+    runde.command_to_event(spieler[0].name, "einwerfen")
+    runde.command_to_event(spieler[1].name, "einwerfen")
 
-# 1 und 2 stechen
-wuerfel.werfen = lambda n: (1,) * n
-runde.command_to_event(spieler_1.name, "stechen")
-wuerfel.werfen = lambda n: (2,) * n
-runde.command_to_event(spieler_2.name, "stechen")
+    with pytest.raises(FalscheAktion):
+        runde.command_to_event(spieler[1].name, events.WÜRFELN)
 
-# spieler 2 versucht unerlaubterweise anzufangen
-with pytest.raises(FalscherSpieler):
-    runde.command_to_event(spieler_2.name, "wuerfeln")
 
-# spieler 1 faengt an
-runde.command_to_event(spieler_1.name, "wuerfeln")
-assert runde.state == "wuerfeln"
-assert runde.einwerfen.spieler_liste[0].name == spieler_1.name
+@pytest.fixture
+def drei_spieler_eingeworfen_spieler_zwei_muss_werfen(spieler):
+    # FALL: spieler 2 würfelt eindeutig das niedrigste
+    runde = SchockenRunde()
+    assert runde.leaf_state.name == "einwerfen"
+    wuerfel.werfen = lambda n: (2,) * n
+    runde.command_to_event(spieler[0].name, "einwerfen")
+    wuerfel.werfen = lambda n: (1,) * n
+    runde.command_to_event(spieler[1].name, "einwerfen")
+    wuerfel.werfen = lambda n: (3,) * n
+    runde.command_to_event(spieler[2].name, "einwerfen")
+    return runde
 
-# FALL einwerfen wenn stechen im gange ist
-runde = SchockenRunde()
 
-wuerfel.werfen = lambda n: (1,) * n
-runde.command_to_event(spieler_1.name, "einwerfen")
-runde.command_to_event(spieler_2.name, "einwerfen")
+def test_falscher_spieler(spieler, drei_spieler_eingeworfen_spieler_zwei_muss_werfen):
+    runde = drei_spieler_eingeworfen_spieler_zwei_muss_werfen
+    # Richtige exception, wenn jemand anders anfangen will?
+    with pytest.raises(FalscherSpieler):
+        runde.command_to_event(spieler[0].name, events.WÜRFELN)
 
-# spieler 2 will nochmal einwerfen
-with pytest.raises(FalscherSpieler):
-    runde.command_to_event(spieler_2.name, "einwerfen")
 
-runde.command_to_event(spieler_1.name, "stechen")
-# weiterer spieler will noch einwerfen, obwohl gestochen wird
-with pytest.raises(FalscheAktion):
-    runde.command_to_event(spieler_3.name, "einwerfen")
+def test_falsche_aktion(spieler, drei_spieler_eingeworfen_spieler_zwei_muss_werfen):
+    runde = drei_spieler_eingeworfen_spieler_zwei_muss_werfen
+    # Richtige exception, wenn jemand stechen will.
+    with pytest.raises(FalscheAktion):
+        runde.command_to_event(spieler[1].name, "stechen")
 
-# FALL Stiche sind größer als nächstkleinere Einwurf Augenzahlen
-runde = SchockenRunde()
 
-assert runde.state == "einwerfen"
-wuerfel.werfen = lambda n: (2,) * n
-runde.command_to_event(spieler_1.name, "einwerfen")
-wuerfel.werfen = lambda n: (3,) * n
-runde.command_to_event(spieler_2.name, "einwerfen")
-wuerfel.werfen = lambda n: (2,) * n
-runde.command_to_event(spieler_3.name, "einwerfen")
+def test_korrekter_start(spieler, drei_spieler_eingeworfen_spieler_zwei_muss_werfen):
+    runde = drei_spieler_eingeworfen_spieler_zwei_muss_werfen
+    # momentaner state ist einwerfen
+    assert runde.leaf_state.name == "einwerfen"
 
-wuerfel.werfen = lambda n: (6,) * n
-runde.command_to_event(spieler_1.name, "stechen")
+    # spieler[1] wuerfelt, danach muss einwerfen fertig sein
+    runde.command_to_event(spieler[1].name, events.WÜRFELN)
+    assert runde.leaf_state.name == events.WÜRFELN
 
-wuerfel.werfen = lambda n: (5,) * n
-runde.command_to_event(spieler_3.name, "stechen")
 
-# spieler 3 muss anfangen zu würfeln
-with pytest.raises(FalscherSpieler):
-    runde.command_to_event(spieler_1.name, "wuerfeln")
-    runde.command_to_event(spieler_2.name, "wuerfeln")
+@pytest.fixture
+def vierfach_stechen(spieler):
+    runde = SchockenRunde()
+    assert runde.leaf_state.name == "einwerfen"
+    wuerfel.werfen = lambda n: (1,) * n
+    for S in spieler:
+        runde.command_to_event(S.name, "einwerfen")
+    return runde
+
+
+def test_mehrfaches_stechen(spieler, vierfach_stechen):
+    # FALL mehrfaches stechen, erst 4, dann 3, dann 2 spieler, dann beginn
+    runde = vierfach_stechen
+
+    # alle stecher in der liste?
+    stecher = [st.name for st in runde.einwerfen.stecher_liste]
+    assert stecher == [S.name for S in spieler]
+
+    # neues stechen, 3 spieler werfen 1, spieler_4 wirft 2
+    runde.command_to_event(spieler[0].name, "stechen")
+    assert runde.leaf_state.name == "stechen"
+    runde.command_to_event(spieler[1].name, "stechen")
+    runde.command_to_event(spieler[2].name, "stechen")
+    wuerfel.werfen = lambda n: (2,) * n
+    runde.command_to_event(spieler[3].name, "stechen")
+    # spieler 4 versucht unerlaubterweise zu stechen
+    with pytest.raises(FalscherSpieler):
+        runde.command_to_event(spieler[3].name, "stechen")
+
+    # die anderen spieler stechen
+    wuerfel.werfen = lambda n: (1,) * n
+    runde.command_to_event(spieler[0].name, "stechen")
+    runde.command_to_event(spieler[1].name, "stechen")
+    wuerfel.werfen = lambda n: (2,) * n
+    runde.command_to_event(spieler[2].name, "stechen")
+    # nur noch 1 und 2 in der stecherliste
+    stecher = [st.name for st in runde.einwerfen.stecher_liste]
+    assert stecher == [S.name for S in spieler[:2]]
+
+    # 1 und 2 stechen
+    wuerfel.werfen = lambda n: (2,) * n
+    runde.command_to_event(spieler[0].name, "stechen")
+    wuerfel.werfen = lambda n: (1,) * n
+    runde.command_to_event(spieler[1].name, "stechen")
+
+    # spieler 1 versucht unerlaubterweise anzufangen
+    with pytest.raises(FalscherSpieler):
+        runde.command_to_event(spieler[0].name, events.WÜRFELN)
+
+    # spieler 2 faengt an
+    runde.command_to_event(spieler[1].name, events.WÜRFELN)
+    assert runde.leaf_state.name == events.WÜRFELN
+    assert runde.einwerfen.sortierte_spieler_liste()[0].name == spieler[1].name
+
+
+def test_einwerfen_während_stechen(spieler):
+    # FALL einwerfen wenn stechen im gange ist
+    runde = SchockenRunde()
+
+    wuerfel.werfen = lambda n: (1,) * n
+    runde.command_to_event(spieler[0].name, "einwerfen")
+    runde.command_to_event(spieler[1].name, "einwerfen")
+
+    # spieler 2 will nochmal einwerfen
+    with pytest.raises(FalscherSpieler):
+        runde.command_to_event(spieler[1].name, "einwerfen")
+
+    runde.command_to_event(spieler[0].name, "stechen")
+    # weiterer spieler will noch einwerfen, obwohl gestochen wird
+    with pytest.raises(FalscheAktion):
+        runde.command_to_event(spieler[2].name, "einwerfen")
+
+
+def test_stiche_höher_einwurf_augen(spieler):
+    # FALL Stiche sind größer als nächstkleinere Einwurf Augenzahlen
+    runde = SchockenRunde()
+
+    assert runde.leaf_state.name == "einwerfen"
+    wuerfel.werfen = lambda n: (2,) * n
+    runde.command_to_event(spieler[0].name, "einwerfen")
+    wuerfel.werfen = lambda n: (3,) * n
+    runde.command_to_event(spieler[1].name, "einwerfen")
+    wuerfel.werfen = lambda n: (2,) * n
+    runde.command_to_event(spieler[2].name, "einwerfen")
+
+    wuerfel.werfen = lambda n: (6,) * n
+    runde.command_to_event(spieler[0].name, "stechen")
+
+    wuerfel.werfen = lambda n: (5,) * n
+    runde.command_to_event(spieler[2].name, "stechen")
+
+    # spieler 3 muss anfangen zu würfeln
+    with pytest.raises(FalscherSpieler):
+        runde.command_to_event(spieler[0].name, events.WÜRFELN)
+        runde.command_to_event(spieler[1].name, events.WÜRFELN)
