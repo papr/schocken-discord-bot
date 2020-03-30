cf31d967560d04af5e5e19f637df28e43e4fc35c
klunkean <andre.klunker@posteo.de>
2020-03-30 13:33:40 +0200

    add dev install to setup, add pytest-asyncio to dev-dependencies

--- setup.py
+++ setup.py
@@ -4,9 +4,15 @@ from setuptools import setup, find_namespace_packages
 package_dir = "src"
 pyproject = toml.load("pyproject.toml")
 
+# install dev using pipenv:
+# pipenv install -e. ".[dev]"
+
 setup(
     **pyproject["project"],
     install_requires=pyproject["dependencies"].keys(),
+    extras_require={
+                    "dev": pyproject["dev-dependencies"].keys()
+                   },
     package_dir={"": package_dir},
     packages=find_namespace_packages(where=package_dir),
 )
