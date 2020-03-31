fe5fb282103b556ad34dcdb183f56de272e5245d
Pablo Prietz <pablo@prietz.org>
2020-03-31 13:57:13 +0200

    Move run.py code to __main__.py

--- src/schocken/__main__.py
+++ src/schocken/__main__.py
@@ -1,9 +1,34 @@
-from .wurf import welcher_wurf
-from . import wuerfel
+import os
+
+import discord
+from discord.utils import get
+from dotenv import load_dotenv
+
+from schocken.exceptions import FalscherServer
+from schocken.bot import SchockenBot
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
+client = discord.Client()
+
+# when connecting
+@client.event
+async def on_ready():
+    client.bot = SchockenBot(client)
+    # check if server is correct:
+    for guild in client.guilds:
+        if guild.name != client.bot.valid_guild_name:
+            raise FalscherServer("Dieser Bot darf nur ins Café A")
+    print("Success")
+    return
 
 
-for _ in range(10):
-    aus_der_hand = bool(wuerfel.randint(0, 1))
-    wuerfel_augen = wuerfel.werfen(3)
-    wurf = welcher_wurf(wuerfel_augen)
-    print(wuerfel_augen, f"{aus_der_hand=}", wurf, wurf.deckel_wert)
+# when a message is read by the bot
+@client.event
+async def on_message(message):
+    await client.bot.parse_input(message)
+    return
+
+
+client.run(TOKEN)
