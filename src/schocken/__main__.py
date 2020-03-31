b81dc4afa3d46adfd12b5719bccc16da602a9a11
Pablo Prietz <pablo@prietz.org>
2020-03-31 14:29:17 +0200

    `schocken` console entrypoint

--- src/schocken/__main__.py
+++ src/schocken/__main__.py
@@ -7,28 +7,32 @@ from dotenv import load_dotenv
 from schocken.exceptions import FalscherServer
 from schocken.bot import SchockenBot
 
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD = os.getenv("DISCORD_GUILD")
-client = discord.Client()
+def run_bot():
+    load_dotenv()
+    TOKEN = os.getenv("DISCORD_TOKEN")
+    GUILD = os.getenv("DISCORD_GUILD")
+    client = discord.Client()
 
-# when connecting
-@client.event
-async def on_ready():
-    client.bot = SchockenBot(client)
-    # check if server is correct:
-    for guild in client.guilds:
-        if guild.name != client.bot.valid_guild_name:
-            raise FalscherServer("Dieser Bot darf nur ins Café A")
-    print("Success")
-    return
+    # when connecting
+    @client.event
+    async def on_ready():
+        client.bot = SchockenBot(client)
+        # check if server is correct:
+        for guild in client.guilds:
+            if guild.name != client.bot.valid_guild_name:
+                raise FalscherServer("Dieser Bot darf nur ins Café A")
 
-
-# when a message is read by the bot
-@client.event
-async def on_message(message):
-    await client.bot.parse_input(message)
-    return
+        ch = client.get_channel(690929770355097610)  # schocktresen
+        await ch.send("Schocken kann jetzt losgehen. :muscle:")
+        print("Success")
+        return
 
 
-client.run(TOKEN)
+    # when a message is read by the bot
+    @client.event
+    async def on_message(message):
+        await client.bot.parse_input(message)
+        return
+
+
+    client.run(TOKEN)
