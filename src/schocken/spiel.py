39a3de44a771f6c409ef420352185844809f5224
Pablo Prietz <pablo@prietz.org>
2020-04-10 18:14:46 +0200

    Fix 96c591551f4388abbfb36f5061f53075c380d651

--- src/schocken/spiel.py
+++ src/schocken/spiel.py
@@ -225,7 +225,7 @@ class Halbzeit(pysm.StateMachine):
         if akt_spieler.anzahl_wuerfe < self.rdm.num_maximale_wuerfe:
             # check if ones were put aside
             if akt_spieler.einsen > 0:
-                wurf = wuerfel.werfen(3 - len(akt_spieler.einsen))
+                wurf = wuerfel.werfen(3 - akt_spieler.einsen)
                 akt_spieler.augen = tuple(
                     sorted(akt_spieler.augen + wurf, reverse=True)
                 )
